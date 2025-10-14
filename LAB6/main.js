fca81834124bb472dd972c4115e7d5f5f64fa6d8
Nidhi Mogatlal <nidhimogatlal@gmail.com>
2025-10-14 15:40:37 +0000

    produtpos LAB6

---
+++ LAB6/main.js
@@ -0,0 +1,25 @@
+import { produtos } from "./produtos.js";
+
+
+//A variáveis
+
+const titulo= document.createElement("article");
+const artigo= document.createElement("article");
+produtos.forEach(produto => {
+
+
+    console.log(produto.title)
+
+
+
+
+})
+
+
+
+
+
+//B funçao
+
+
+//C declarar
