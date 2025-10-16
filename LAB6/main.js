0a6956245f25a7721227378bd215d66a7330ce50
Nidhi Mogatlal <nidhimogatlal@gmail.com>
2025-10-16 15:44:10 +0000

    fiz a função criarProduto

--- LAB6/main.js
+++ LAB6/main.js
@@ -1,19 +1,68 @@
 import { produtos } from "./produtos.js";
 
 
-//A variáveis
+document.addEventListener('DOMContentLoaded', function() {carregarProdutos(produtos);});
 
-const titulo= document.createElement("article");
-const artigo= document.createElement("article");
-produtos.forEach(produto => {
+//Defenir função 
+
+function carregarProdutos(produtos){
+
+     console.log("Função carregarProdutos foi chamada!");
+    const sectionProdutos=document.getElementById("produtos");
+
+    produtos.forEach(
+        
+        produto=>{
+            const artigo = criarProduto(produto);
+            sectionProdutos.append(artigo);
+        }
+    )
+}
 
 
-    console.log(produto.title)
+//criar função
+
+function criarProduto(produto){
+
+    const artigo= document.createElement("article");
+
+    const titulo= document.createElement("h3");
+    titulo.textContent=produto.title;
+
+
+
+    const secImg= document.createElement("section");
+    const imagem= document.createElement("img");
+    imagem.src=produto.image;
+    secImg.append(imagem);
+
+
+    const preco=  document.createElement("p")
+    preco.textContent=`Custo total: ${produto.price} €`;
+
+    const descricao= document.createElement("p");
+    descricao.textContent=produto.description
+
+    const botao= document.createElement("button");
+    botao.textContent="+ Adicionar ao Cesto";
+
+    botao.addEventListener("click",function(){adicionarProdutoCesto(produto);});
+
+
+    artigo.append(titulo,secImg,preco,descricao,botao);
+
+    return artigo;
+
+}
 
 
 
 
-})
+
+
+
+
+
 
 
 
